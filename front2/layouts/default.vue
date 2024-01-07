fbc70a8a65aa3cb58db6df0d9b6cd73030d5cc0e
nagaminejun <nagaminejun1988@gmail.com>
2024-01-07 17:41:06 +0900

    SPA開発

---
+++ front2/layouts/default.vue
@@ -0,0 +1,77 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer v-model="drawer" :mini-variant="miniVariant" :clipped="clipped" fixed app>
+      <v-list>
+
+        <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact>
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+          </v-list-item-content>
+        </v-list-item>
+
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-content>
+      <router-view />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    name: "LayoutDefault",
+    data() {
+      return {
+        clipped: false,
+        drawer: false,
+        fixed: false,
+        miniVariant: false,
+        right: true,
+        rightDrawer: false,
+        title: 'TODO App'
+      }
+    },
+    computed: {
+      user() {
+        return this.$store.state.auth.currentUser;
+      },
+      items() {
+        if (this.user) {
+          return [{
+            icon: "mdi-apps",
+            title: "TODOS",
+            to: "/"
+          },
+          {
+            icon: "mdi-chart-bubble",
+            title: "MYPAGE",
+            to: "/mypage"
+          }
+          ];
+        } else {
+          return [{
+            icon: "mdi-apps",
+            title: "LOGIN",
+            to: "/login"
+          },
+          {
+            icon: "mdi-chart-bubble",
+            title: "SIGNUP",
+            to: "/signup"
+          }
+          ];
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+  .container {
+    margin: 0 auto;
+    max-width: 800px;
+  }
+</style>z
---
+++ front2/pages/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div v-if="user">
+    <p>名前：{{user.name}}</p>
+    <AddTodo @submit="addTodo" />
+    <TodoList :todos="todos" />
+  </div>
+</template>
+
+<script>
+import AddTodo from "@/components/AddTodo";
+import TodoList from "@/components/TodoList";
+import axios from "@/plugins/axios";
+
+export default {
+  components: {
+    AddTodo,
+    TodoList,
+  },
+  data() {
+    return {
+      todos: [],
+    };
+  },
+  computed: {
+    user() {
+      return this.$store.state.auth.currentUser
+    }
+  },
+  created() {
+    console.log("API_KEY:", process.env.API_KEY);
+  },
+  methods: {
+    async addTodo(title) { // async 非同期
+      await axios.post("/v1/todos", { title }); // await 待つ
+      this.todos.push({
+        title,
+      });
+    },
+  },
+};
+</script>
+
+<style>
+</style>
